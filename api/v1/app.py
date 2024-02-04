91b82cd328bf755e948a0ba7db3c05980be1a3f3
Ehab Elsayed <ehabsmh3@gmail.com>
2024-02-05 00:44:47 +0200

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,8 +4,7 @@
 from os import getenv
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
-
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -15,6 +14,11 @@ app.register_blueprint(app_views)
 def close_storage(error):
     """Closes the storage session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_notfound(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
