89f255e81de6a673d69cc9cbc68c95aa328c0bac
nezhataghy <nezhataghy@gmail.com>
2024-02-05 21:54:42 +0000

    pycodestyle

--- tests/test_api/test_v1/test_app.py
+++ tests/test_api/test_v1/test_app.py
@@ -9,10 +9,11 @@ class Test_App_Docs(unittest.TestCase):
     def test_module_doc(self):
         app_doc = app.__doc__
         self.assertTrue(app_doc)
-        
+
     def test_fn_doc(self):
         fn_doc = app.close_storage.__doc__
         self.assertTrue(fn_doc)
+
 
 if __name__ == "__main__":
     unittest.main()
