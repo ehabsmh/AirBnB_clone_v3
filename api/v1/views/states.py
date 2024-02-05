5ab780909725a62499bfc06798c38466ea436736
nezhataghy <nezhataghy@gmail.com>
2024-02-05 16:23:22 +0000

    state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """handles all default RESTFul API actions for State objects"""
+
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
